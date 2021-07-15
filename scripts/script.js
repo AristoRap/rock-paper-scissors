27d0500ee12cc38037876493caad955db4a04025
AristoRap <65190225+AristoRap@users.noreply.github.com>
2021-07-15 22:58:35 +0200

    Added missed let declaration within function computerPlay. Added comments for second function to be built

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
 //Begin with a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 function computerPlay() {
-  randomNumberFromOneToThree = Math.floor(Math.random() * 3) + 1;
+  let randomNumberFromOneToThree = Math.floor(Math.random() * 3) + 1;
   if (randomNumberFromOneToThree === 1) return "Rock";
   if (randomNumberFromOneToThree === 2) return "Paper";
   if (randomNumberFromOneToThree === 3) return "Scissors";
+}
+//Write a function that plays a single round of Rock Paper Scissors. 
+//The function should take two parameters - the playerSelection and computerSelection
+//then return a string that declares the winner of the round
+
+function playGame(playerSelection, computerSelection){
+
 }
